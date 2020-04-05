a02691d1bd6c548bde5efa84292696f662eb1ece
gpstrutz <giovannistrutz97@gmail.com>
2020-04-05 19:56:09 -0300

    Add the main file with database settings

--- database/models.py
+++ database/models.py
@@ -1,6 +1,5 @@
 from .db import database
 
-
 class User(database.Document):
     name = database.StringField(required=True, unique=True)
     total_value = database.DecimalField(precision=2, force_string=False, required=True)
